f927c639bbeb632f265093e5d9a32e12f648e440
DevilDantes <160239574+DevilDantes@users.noreply.github.com>
2024-05-27 08:25:08 -0500

    Update main.py

--- LoginApp/main.py
+++ LoginApp/main.py
@@ -3,3 +3,5 @@ from formularios.login import *
 if __name__ == '__main__':
     app = Login()
     app.mainloop()
+
+#Juan David Pedraza Valencia, Kener Safat Theran Revolledo, Jesus javier Sanchez Vanegas, Douglas Josue Araujo Mendez, Fabian Andres Perez Melendez
